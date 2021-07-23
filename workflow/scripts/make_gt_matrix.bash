d4a0ebb72f2900628edcd18cb9d285c86417b9df
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2021-07-23 09:44:26 -0700

    tranpose gt matrix and add some extra fluff to the Snakefile

--- workflow/scripts/make_gt_matrix.bash
+++ workflow/scripts/make_gt_matrix.bash
@@ -2,17 +2,17 @@
 
 # arg1: the superpopulation to sample from
 # arg2: the minimum minor allele frequency
-# arg3: VCF file from which to obtain genotypes
-# arg4: samples file from 1000G website (contains columns: Sample name|Sex|Biosample ID|Population code|Population name|Superpopulation code|Superpopulation name|Population elastic ID|Data collections)
+# arg3: the locus from which to obtain the genotypes
+# arg4: VCF file from which to obtain genotypes
+# arg5: samples file from 1000G website (contains columns: Sample name|Sex|Biosample ID|Population code|Population name|Superpopulation code|Superpopulation name|Population elastic ID|Data collections)
 # ex: workflow/scripts/make_genotype_matrix.bash EUR 0.1 /projects/ps-gymreklab/resources/datasets/1000Genomes/phase3VCFs/ALL.chr1.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz data/igsr_samples.tsv
-
-
 
 
 samp="$1"
 min_maf="$2"
-vcf_1000g="$3"
-samps_file="$4"
+loc="$3"
+vcf_1000g="$4"
+samps_file="$5"
 
 # zcat "$vcf_1000g" 2>/dev/null | \
 #     grep -E -m1 '^#CHROM' | \
@@ -29,10 +29,13 @@ comm -12 <(
 ) <(
     awk -F $'\t' -v 'OFS=\t' '$6 == "'"$samp"'" {print $1;}' "$samps_file" | \
     sort
-) | \
-paste -s -d $'\t' | \
+) | {
+    echo -ne "ID\t"
+    paste -s -d $'\t'
+} | \
 tee >(
-    bcftools view --min-af "$min_maf":minor --samples-file <(tr $'\t' '\n') "$vcf_1000g" | \
-    bcftools query -f '[%GT\t]\n' | \
+    bcftools view --min-af "$min_maf":minor --samples-file <(tr $'\t' '\n' | tail -n+2) "$vcf_1000g" "$loc" | \
+    bcftools query -f '%CHROM\_%POS\t[%GT\t]\n' | \
     sed 's/\t$//; s/0|0/0/g; s/0|1/1/g; s/1|0/1/g; s/1|1/2/g'
-)
+) | \
+datamash transpose
