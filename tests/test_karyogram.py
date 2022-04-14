b13a5d73b283e77b04e72480001cdc4f103f6adb
Melissa Gymrek <melissagymrek@Melissas-MacBook-Pro.local>
2022-04-14 11:50:44 -0700

    cleaning up karyogram user interface

--- tests/test_karyogram.py
+++ tests/test_karyogram.py
@@ -28,8 +28,3 @@ def test_GetHaplotypeBlocks():
 	assert(sample_blocks[1][0]["chrom"]==1)
 	assert(sample_blocks[1][0]["start"]==0.0001)
 	assert(sample_blocks[1][0]["end"]==85.107755)
-
-test_file = DATADIR.joinpath("5gen.bp")
-centromeres = DATADIR.joinpath("centromeres_hg19.txt")
-PlotKaryogram(test_file, "Sample_1", "test.png",
-       centromeres_file=centromeres, title=None)
