0b1ad1ca84771d806d21bec93bdb5e767713b891
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2022-08-03 18:37:38 +0200

    add --from-gts switch to ld command

--- haptools/ld.py
+++ haptools/ld.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 import logging
 from pathlib import Path
+from fileinput import hook_compressed
 from dataclasses import dataclass, field
 
 import numpy as np
+import numpy.typing as npt
 
 from haptools import data
 from .data import Haplotype as HaplotypeBase
@@ -26,15 +28,35 @@ class Haplotype(HaplotypeBase):
     )
 
 
+def pearson_corr_ld(arrA: npt.NDArray, arrB: npt.NDArray) -> float:
+    """
+    Compute the Pearson correlation coefficient between two vectors (1D numpy arrays)
+
+    Parameters
+    ----------
+    arrA: npt.NDArray
+        The first 1D numpy array
+    arrB: npt.NDArray
+        The second 1D numpy array
+
+    Returns
+    -------
+    The LD between the genotypes in arrA and the genotypes in arrB
+    """
+    return np.corrcoef(arrA, arrB)[1, 0]
+
+
+
 def calc_ld(
     target: str,
     genotypes: Path,
     haplotypes: Path,
     region: str = None,
     samples: list[str] = None,
-    haplotype_ids: set[str] = None,
+    ids: set[str] = None,
     chunk_size: int = None,
     discard_missing: bool = False,
+    from_gts: bool = False,
     output: Path = Path("/dev/stdout"),
     log: Logger = None,
 ):
@@ -54,10 +76,14 @@ def calc_ld(
         and :py:meth:`~.data.Haplotypes.read`
     samples : list[str], optional
         See documentation for :py:meth:`~.data.Genotypes.read`
-    haplotype_ids: set[str], optional
-        A set of haplotype IDs to obtain from the .hap file. All others are ignored.
+    ids: set[str], optional
+        A subset of haplotype IDs to obtain from the .hap file. All others
+        are ignored.
 
-        If not provided, all haplotypes will be used.
+        Alternatively, if the --from-gts switch is specified, this will be interpreted
+        as a subset of variant IDs to obtain from the genotypes file.
+
+        Defaults to loading all haplotypes or variants if not specified
     chunk_size: int, optional
         The max number of variants to fetch from the PGEN file at any given time
 
@@ -82,25 +108,38 @@ def calc_ld(
 
     log.info("Loading haplotypes")
     hp = data.Haplotypes(haplotypes, log=log)
-    if haplotype_ids is not None:
-        haplotype_ids.add(target)
+    haplotype_ids = None
+    if not from_gts:
+        haplotype_ids = ids
+        if haplotype_ids is not None:
+            haplotype_ids.add(target)
     hp.read(region=region, haplotypes=haplotype_ids)
 
-    log.info("Extracting variants from haplotypes")
-    variants = {var.id for hap in hp.data.values() for var in hap.variants}
+    if from_gts:
+        variants = ids or set()
+        if target in hp.data:
+            log.info("Extracting variants from haplotypes")
+            variants.update(
+                var.id for hap in hp.data.values()
+                for var in hap.variants if hap.id == target
+            )
+    else:
+        log.info("Extracting variants from haplotypes")
+        variants = {var.id for hap in hp.data.values() for var in hap.variants}
 
     # check to see whether the target was a haplotype
     if target in hp.data:
+        log.info(f"Identified target '{target}' as a haplotype")
         target = hp.data.pop(target)
-        if len(hp.data) == 0:
+        if len(hp.data) == 0 and not from_gts:
             log.error(
                 "There must be at least one more haplotype in the .hap file "
                 "than the TARGET haplotype specified."
             )
 
     # check that all of the haplotypes were loaded successfully and warn otherwise
-    if haplotype_ids is not None and len(haplotype_ids) > len(hp.data):
-        diff = list(haplotype_ids.difference(hp.data.keys()))
+    if ids is not None and len(ids) > len(hp.data):
+        diff = list(ids.difference(hp.data.keys()))
         first_few = 5 if len(diff) > 5 else len(diff)
         log.warning(
             f"{len(diff)} haplotypes could not be found in the .hap file. Check "
@@ -134,6 +173,7 @@ def calc_ld(
                 "in the .hap file or the variants in the genotype file. Check that "
                 f"'{target}' appears in either the .hap file or the genotype file."
             )
+        log.info(f"Identified target '{target}' as a variant")
         # report the missing variants
         diff = list(variants.difference(gt.variants["id"]))
         first_few = 5 if len(diff) > 5 else len(diff)
@@ -143,9 +183,10 @@ def calc_ld(
             f"file. Here are the first few missing variants: {diff[:first_few]}"
         )
 
-    log.info("Transforming genotypes via haplotypes")
-    hp_gt = data.GenotypesRefAlt(fname=None, log=log)
-    hp.transform(gt, hp_gt)
+    if not from_gts:
+        log.info("Transforming genotypes via haplotypes")
+        hp_gt = data.GenotypesRefAlt(fname=None, log=log)
+        hp.transform(gt, hp_gt)
 
     log.info("Obtaining target genotypes")
     if isinstance(target, data.Haplotype):
@@ -153,21 +194,31 @@ def calc_ld(
     else:
         target_gts = gt.subset(variants=(target,)).data[:, 0, :2].sum(axis=1)
 
-    log.info("Computing LD between haplotypes and the target")
-    # construct a new Haplotypes object that also stores the LD values
-    hp_out = data.Haplotypes(fname=output, haplotype=Haplotype, log=log)
-    hp_out.data = {}
-    for hap_id in hp.data:
-        # break the BaseHaplotype instance up into its properties
-        hapd = hp.data[hap_id].__dict__
-        hapd_variants = hapd.pop("variants")
-        # obtain genotypes for the current haplotype
-        hap_gts = hp_gt.subset(variants=(hap_id,)).data[:, 0, :2].sum(axis=1)
-        # compute the LD between the current haplotype and the target
-        hapd["ld"] = np.corrcoef(target_gts, hap_gts)[1, 0]
-        # create the Haplotype instance and add the variants in
-        hp_out.data[hap_id] = Haplotype(**hapd)
-        hp_out.data[hap_id].variants = hapd_variants
-
-    log.info("Outputting .hap file with LD values")
-    hp_out.write()
+    if from_gts:
+        log.info("Computing LD between genotypes and the target")
+        with hook_compressed(output, mode="wt") as ld_file:
+            log.info("Outputting .ld file with LD values")
+            ld_file.write("CHR\tBP\tSNP\tR\n")
+            for idx, variant in enumerate(gt.variants[["chrom", "pos", "id"]]):
+                var_chr, var_bp, var_snp = variant
+                variant_gts = gt.data[:, idx, :2].sum(axis=1)
+                variant_ld = pearson_corr_ld(target_gts, variant_gts)
+                ld_file.write(f"{var_chr}\t{var_bp}\t{var_snp}\t{variant_ld:.3f}\n")
+    else:
+        log.info("Computing LD between haplotypes and the target")
+        # construct a new Haplotypes object that also stores the LD values
+        hp_out = data.Haplotypes(fname=output, haplotype=Haplotype, log=log)
+        hp_out.data = {}
+        for hap_id in hp.data:
+            # break the BaseHaplotype instance up into its properties
+            hapd = hp.data[hap_id].__dict__
+            hapd_variants = hapd.pop("variants")
+            # obtain genotypes for the current haplotype
+            hap_gts = hp_gt.subset(variants=(hap_id,)).data[:, 0, :2].sum(axis=1)
+            # compute the LD between the current haplotype and the target
+            hapd["ld"] = pearson_corr_ld(target_gts, hap_gts)
+            # create the Haplotype instance and add the variants in
+            hp_out.data[hap_id] = Haplotype(**hapd)
+            hp_out.data[hap_id].variants = hapd_variants
+        log.info("Outputting .hap file with LD values")
+        hp_out.write()
