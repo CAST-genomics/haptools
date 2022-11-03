13c0a25a1499fc627e0bda2008b7a95971f1f607
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2022-11-03 10:47:00 -0700

    refmt with black

--- haptools/logging.py
+++ haptools/logging.py
@@ -5,7 +5,7 @@ import logging
 def getLogger(name: str = None):
     """
     Retrieve a Logger object
-    
+
     Parameters
     ----------
     name : str, optional
@@ -14,7 +14,7 @@ def getLogger(name: str = None):
     if name is None:
         pass
 
-    log = logging.getLogger("haptools "+name)
+    log = logging.getLogger("haptools " + name)
     db_time = "|%(asctime)s" if verbosity == "DEBUG" else ""
     logging.basicConfig(
         format="[%(levelname)8s" + db_time + "] %(message)s (%(filename)s:%(lineno)s)",
