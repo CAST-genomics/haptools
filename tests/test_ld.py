24248371a93ac5b41e60f67376ea64852c3439da
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2022-12-29 12:30:08 -0800

    finish testing all examples in the LD command docs

--- tests/test_ld.py
+++ tests/test_ld.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import numpy as np
 from click.testing import CliRunner
 
+from haptools.data import Data
 from haptools.__main__ import main
 
 DATADIR = Path(__file__).parent.joinpath("data")
@@ -37,13 +38,19 @@ H\t19\t45411941\t45412079\tAPOe4\t0.999
 V\tAPOe4\t45411941\t45411941\trs429358\tC
 V\tAPOe4\t45412079\t45412079\trs7412\tC
 """
+    tmp_file = Path("apoe4_ld.hap")
 
-    cmd = "ld rs429358 tests/data/apoe.vcf.gz tests/data/apoe4.hap"
+    cmd = f"ld -o {tmp_file} rs429358 tests/data/apoe.vcf.gz tests/data/apoe4.hap"
     runner = CliRunner()
     result = runner.invoke(main, cmd.split(" "))
     captured = capfd.readouterr()
-    assert captured.out == expected
+    assert captured.out == ""
     assert result.exit_code == 0
+
+    with Data.hook_compressed(tmp_file, mode="r") as haps:
+        assert haps.read() == expected
+
+    tmp_file.unlink()
 
 
 def test_from_gts(capfd):
@@ -57,13 +64,19 @@ def test_from_gts(capfd):
 19\t45412040\trs769455\t0.006
 19\t45412079\trs7412\t-0.098
 """
+    tmp_file = Path("apoe4.ld")
 
-    cmd = "ld --from-gts APOe4 tests/data/apoe.vcf.gz tests/data/apoe4.hap"
+    cmd = "ld --from-gts -o apoe4.ld APOe4 tests/data/apoe.vcf.gz tests/data/apoe4.hap"
     runner = CliRunner()
     result = runner.invoke(main, cmd.split(" "))
     captured = capfd.readouterr()
-    assert captured.out == expected
+    assert captured.out == ""
     assert result.exit_code == 0
+
+    with Data.hook_compressed(tmp_file, mode="r") as snps:
+        assert snps.read() == expected
+
+    tmp_file.unlink()
 
 
 def test_from_gts_ids(capfd):
