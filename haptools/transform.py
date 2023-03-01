8a9ff58c9c67d58a8514078cf77f6af6ff49f4d0
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2023-03-01 09:57:35 -0800

    fix remaining test errors

--- haptools/transform.py
+++ haptools/transform.py
@@ -98,7 +98,7 @@ class HaplotypesAncestry(data.Haplotypes):
             hap_gts = data.GenotypesRefAlt(fname=None, log=self.log)
         hap_gts.samples = gts.samples
         hap_gts.variants = np.array(
-            [(hap.id, hap.chrom, hap.start, "A", "T") for hap in self.data.values()],
+            [(hap.id, hap.chrom, hap.start, ("A", "T")) for hap in self.data.values()],
             dtype=hap_gts.variants.dtype,
         )
         # build a fast data structure for querying the alleles in each haplotype:
