b86fea808fbe194d39919cda459f899653f88c52
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2024-11-07 07:05:15 -0800

    ci: add support for python 3.13 (#261)

--- noxfile.py
+++ noxfile.py
@@ -9,7 +9,7 @@ from nox_poetry import session
 
 
 package = "haptools"
-python_versions = ["3.7", "3.8", "3.9", "3.10", "3.11", "3.12"]
+python_versions = ["3.7", "3.8", "3.9", "3.10", "3.11", "3.12", "3.13"]
 locked_python_version = "3.8"
 nox.needs_version = ">= 2022.11.21"
 nox.options.sessions = (
@@ -45,7 +45,7 @@ def install_handle_python_numpy(session):
     handle incompatibilities with python and numpy versions
     see https://github.com/cjolowicz/nox-poetry/issues/1116
     """
-    if session._session.python in ["3.11", "3.12"]:
+    if session._session.python in ["3.11", "3.12", "3.13"]:
         session._session.install(".")
     else:
         session.install(".")
