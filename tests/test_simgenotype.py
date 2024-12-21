78ec92d7d66e2d07fed80012f208148cfa921d4f
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2024-12-21 05:49:25 +0000

    add test of chunked output

--- tests/test_simgenotype.py
+++ tests/test_simgenotype.py
@@ -74,3 +74,38 @@ def test_pgen_output(capfd):
     prefix.unlink()
     prefix.with_suffix(".pvar").unlink()
     prefix.with_suffix(".psam").unlink()
+
+
+def test_pgen_output_chunked(capfd):
+    prefix = DATADIR / "example_simgenotype.pgen"
+    dat_file = DATADIR / "outvcf_gen.dat"
+    map_dir = DATADIR / "map"
+    ref_vcf_file = DATADIR / "outvcf_test.pgen"
+    samp_info_file = DATADIR / "outvcf_info.tab"
+
+    cmd = " ".join(
+        [
+            "simgenotype",
+            f"--model {dat_file}",
+            f"--mapdir {map_dir}",
+            "--region 1:1-83000",
+            f"--ref_vcf {ref_vcf_file}",
+            f"--sample_info {samp_info_file}",
+            f"--out {prefix}",
+            f"--chunk-size 1"
+        ]
+    )
+    runner = CliRunner()
+    result = runner.invoke(main, cmd.split(" "), catch_exceptions=False)
+    captured = capfd.readouterr()
+    assert result.exit_code == 0
+    assert prefix.with_suffix(".bp").exists()
+    assert prefix.exists()
+    assert prefix.with_suffix(".pvar").exists()
+    assert prefix.with_suffix(".psam").exists()
+
+    # delete the files and directory we just created
+    prefix.with_suffix(".bp").unlink()
+    prefix.unlink()
+    prefix.with_suffix(".pvar").unlink()
+    prefix.with_suffix(".psam").unlink()
